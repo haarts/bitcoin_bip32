028af554c39195daaeb107d571b221150d13d082
Harm Aarts <harmaarts@gmail.com>
2018-10-30 14:04:25 +0100

    Fix example

--- example/main.dart
+++ example/main.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:convert/convert.dart';
-import "package:bip32/bip32.dart";
+import "package:bitcoin_bip32/bitcoin_bip32.dart";
 
 void main() {
   Chain chain = Chain.seed(hex.encode(utf8.encode("some seed")));
